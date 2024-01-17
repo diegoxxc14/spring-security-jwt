a7c235e76daa2411ec89e64902b148948690def9
Diego Cuenca <dcuenca@lorente.fin.ec>
2024-01-17 16:58:07 -0500

    Crear la relación entre UserEntity y RoleEntity

--- src/main/java/com/demo/models/UserEntity.java
+++ src/main/java/com/demo/models/UserEntity.java
@@ -1,9 +1,16 @@
 package com.demo.models;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -35,4 +42,11 @@ public class UserEntity {
 
     @NotBlank
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER,  // Trae todos los roles
+        targetEntity = RoleEntity.class, cascade = CascadeType.PERSIST)  // Solo cuando se cree un usuario
+    @JoinTable(name = "user_roles",  // Nombre de la nueva tabla
+        joinColumns = @JoinColumn(name = "user_id"),  // FK de UserEntity
+        inverseJoinColumns = @JoinColumn(name = "role_id"))  // FK de RoleEntity
+    private Set<RoleEntity> roles;
 }

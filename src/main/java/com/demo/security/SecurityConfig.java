82d2234b6c0d7852aff5fcb6e10ec52417e12afa
Diego Cuenca <dcuenca@lorente.fin.ec>
2024-01-18 11:47:47 -0500

    Implementación de la Spring Security

---
+++ src/main/java/com/demo/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .csrf(config -> config.disable())
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/hello").permitAll();
+                auth.anyRequest().authenticated();
+            })
+            .sessionManagement(session -> {
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            })
+            .httpBasic()
+            .and()
+            .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        // Crear un usuario en memoria
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("diego")
+            .password("1234")
+            .roles()
+            .build());
+
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
+        // Administrar la autenticación
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+            .userDetailsService(userDetailsService())
+            .passwordEncoder(passwordEncoder)
+            .and().build();
+    }
+}
